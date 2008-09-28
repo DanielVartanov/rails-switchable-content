5b9d0946d06b34b1943b46ed653f3fb3410b08c6
Daniel Vartanov <daniel.vartanov@gmail.com>
2008-09-28 14:24:25 +0600

    Now using TextHelper.concat and CaptureHelper.capture to capture multiple calls in a block

--- lib/switchable_content.rb
+++ lib/switchable_content.rb
@@ -1,16 +1,17 @@
 module ActionView
   module Helpers
     module SwitchableContent
-      def switchable_content
+      def switchable_content(&block)
         block_key = Digest::SHA1.hexdigest( Time.now.to_s.split(//).sort_by {rand}.join )
         block_id = "switchable_block_#{block_key}"
 
-        return <<HTML
-        <input type="checkbox" onchange="block = document.getElementById('#{block_id}'); if (this.checked) {block.style.display = 'block'} else {block.style.display = 'none'}" />
-        <div id="#{block_id}" style="display:block">
-          #{yield}
-        </div>
+        html = <<HTML
+<input type="checkbox" checked onchange="block = document.getElementById('#{block_id}'); if (this.checked) {block.style.display = 'block'} else {block.style.display = 'none'}" />
+<div id="#{block_id}" style="display:block">
+  #{capture(&block)}
+</div>
 HTML
+        html.each { |line| concat(line, block.binding) }
       end
     end
   end
